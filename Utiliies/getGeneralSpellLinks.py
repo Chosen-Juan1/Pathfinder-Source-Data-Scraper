132657e1b216731c9f061a16009af3f33c8c5ccc
Chosen-Juan1 <juan.guadalupe@upr.edu>
2026-07-06 23:52:48 -0400

    made it so the link scraper ignores the base link it used to get all the other links

--- Utiliies/getGeneralSpellLinks.py
+++ Utiliies/getGeneralSpellLinks.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-def getLinks(selector:str, beautyLinks:BeautifulSoup):
+def getLinks(selector:str, beautyLinks:BeautifulSoup, baseLink:str):
     links = beautyLinks.select(selector)
     listOfLinks = []
     for link in links:
@@ -9,14 +9,17 @@ def getLinks(selector:str, beautyLinks:BeautifulSoup):
         link = str(link).split("\"")
         for segments in link:
             if "http" in segments:
+                if(segments == baseLink):
+                    continue
                 listOfLinks.append(segments)
                 #  print(segments)
     # print(listOfLinks)
     return listOfLinks
 if __name__ == "__main__":
-    response = requests.get("https://www.d20pfsrd.com/magic/all-spells")
+    baseLink = "https://www.d20pfsrd.com/magic/all-spells"
+    response = requests.get(baseLink)
     selector = "[href*='https://www.d20pfsrd.com/magic/all-spells/']"
     beautyLink = BeautifulSoup(response.text,'html.parser')
 
-    listOfLinks = getLinks(selector, beautyLink)
+    listOfLinks = getLinks(selector, beautyLink, baseLink)
     print(listOfLinks)
